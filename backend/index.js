859fb53aa4369d3fe4eaf5e8d284e728a3c39440
codewithfatima <fatima68.05.07@gmail.com>
2024-08-19 11:45:42 +0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+// MongoDB connection
+const MONGO = process.env.MONGO;
+const app = express();
+const port = process.env.PORT ||  5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+const resultSchema = new mongoose.Schema({
+  name: String,
+  score: Number,
+  totalScore: Number,
+  remarks: String,
+});
+
+const Result = mongoose.model('Result', resultSchema);
+
+app.post('/saveResult', async (req, res) => {
+  const { name, score, totalScore, remarks } = req.body;
+  try {
+    const newResult = new Result({ name, score, totalScore, remarks });
+    await newResult.save();
+    res.status(201).json(newResult);
+  } catch (error) {
+    res.status(400).json({ error: 'Error saving result' });
+  }
+});
+
+// Fetch results from the database
+app.get('/results', async (req, res) => {
+  try {
+    const results = await Result.find();
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching results' });
+  }
+});
+app.get('/',(req, res) =>{
+    try{
+    res.json("Get Request")
+    }catch(error){
+        res.json(error);
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ frontend/src/components/Result.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './result.css'
+const userName = localStorage.getItem('userName');
+
+const Results = () => {
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/results');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setResults(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchResults();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+
+    return (
+        <div>
+            <button className="bte" onClick={() => navigate('/')}>Restart Quiz</button>
+            {results.length === 0 ? (
+                <p>No results available</p>
+            ) : (
+                <table >
+                    <thead className='table-header'>
+                        <tr>
+                            <th>Name </th>
+                            <th>Score</th>
+                            <th>Total Score</th>
+                            <th>Remarks</th>
+                        </tr>
+                    </thead>
+                    <tbody className='table-body'>
+                        {results.map((result) => (
+                            <tr key={result.id}>
+                                <td>{result.name}</td>
+                                <td>{result.score}</td>
+                                <td>{result.totalScore}</td>
+                                <td>{result.remarks}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
+};
+
+export default Results;
+
+
+ 
