c95019a5cea0f6a9875eedeb9986c80dff80f182
codewithfatima <fatima68.05.07@gmail.com>
2024-08-20 19:55:25 +0300

    update

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
-const { fileURLToPath } = require('url');
+import { fileURLToPath } from 'url';
 
 
 const __filename = fileURLToPath(import.meta.url)
