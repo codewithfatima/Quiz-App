2f0db756162abf2ef4df67ead5eb82ef5021657a
codewithfatima <fatima68.05.07@gmail.com>
2024-08-21 09:49:32 +0300

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,16 @@
 //   plugins: [react()],
 //   // Other Vite configuration options
 // });
+
+
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    // Add other build options here
+    
   },
-  // Add other Vite configurations here
 });
+
